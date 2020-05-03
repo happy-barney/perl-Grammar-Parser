e464fc8c462378c8545f82ffbebc2cd6c4fbbc7a
Branislav Zahradník <happy.barney+perl-Grammar-Parser@gmail.com>
2020-05-04 01:48:15 +0200

    Language::Java - enum declaration in annotation body

--- lib/CSI/Language/Java/Grammar.pm
+++ lib/CSI/Language/Java/Grammar.pm
@@ -451,6 +451,7 @@ package CSI::Language::Java::Grammar v1.0.0 {
 		[qw[  class_declaration               ]],
 		[qw[  constant_declaration            ]],
 		[qw[  empty_declaration               ]],
+		[qw[  enum_declaration                ]],
 		[qw[  interface_declaration           ]],
 		;
 
