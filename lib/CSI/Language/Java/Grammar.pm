94993feaa0b4b5445c69543b68168756f293164b
Branislav Zahradník <happy.barney+perl-Grammar-Parser@gmail.com>
2020-05-04 12:00:05 +0200

    Language::Java - favor final '>' to be type list close token

--- lib/CSI/Language/Java/Grammar.pm
+++ lib/CSI/Language/Java/Grammar.pm
@@ -378,9 +378,17 @@ package CSI::Language::Java::Grammar v1.0.0 {
 
 	ensure_rule_name_order;
 
-	rule  TYPE_LIST_CLOSE                   => dom => '::Token::Type::List::Close',
+	rule  TYPE_LIST_CLOSE                   =>
+		[qw[  TYPE_LIST_CLOSE_AMBIGUOUS  ]],
+		[qw[  TYPE_LIST_CLOSE_FINAL      ]],
+		;
+
+	rule  TYPE_LIST_CLOSE_AMBIGUOUS         => dom => '::Token::Type::List::Close',
 		[qw[  TOKEN_GT_AMBIGUOUS  ]],
-		[qw[  TOKEN_GT_FINAL      ]],
+		;
+
+	rule  TYPE_LIST_CLOSE_FINAL             => dom => '::Token::Type::List::Close',
+		[qw[  TOKEN_GT_FINAL  PRIORITY_TOKEN  ]],
 		;
 
 	rule  TYPE_LIST_OPEN                    => dom => '::Token::Type::List::Open',
