8c7cd4fc052170566b3b725b1c3b5fdab828986f
Branislav Zahradník <happy.barney+perl-Grammar-Parser@gmail.com>
2020-05-04 12:20:17 +0200

    Language::Java - regression / floating literal decimal can start with leading zeros

--- lib/CSI/Language/Java/Grammar.pm
+++ lib/CSI/Language/Java/Grammar.pm
@@ -178,7 +178,7 @@ package CSI::Language::Java::Grammar v1.0.0 {
 			(?:
 				(?<value>
 					(?= \.? [[:digit:]] )
-					(??{ 'Decimal_Numeral' })?
+					0* (??{ 'Decimal_Numeral' })?
 					\.
 					0* (??{ 'Decimal_Numeral' }) ?
 					(??{ 'Exponent_Part'   }) ?
@@ -188,7 +188,7 @@ package CSI::Language::Java::Grammar v1.0.0 {
 			|
 			(?:
 				(?<value>
-					(??{ 'Decimal_Numeral' })
+					0* (??{ 'Decimal_Numeral' })
 					(??{ 'Exponent_Part'   })
 					(?<type_suffix> (??{ 'Floating_Type_Suffix' }) ) ?
 				)
@@ -241,6 +241,7 @@ package CSI::Language::Java::Grammar v1.0.0 {
 	token LITERAL_INTEGRAL_OCTAL            => dom => '::Number::Integral::Octal', action => 'integral_value',
 		qr/(?>
 			(?<octal_value>   (??{ 'Octal_Numeral'   }) )
+			(?! \. )
 			(?<type_suffix>   (??{ 'Integral_Type_Suffix' }) )?
 			\b
 		)/sx;
