18dd73c921a5c7e50f2e0bd6f782b7972d3bed8a
Branislav Zahradník <happy.barney+perl-Grammar-Parser@gmail.com>
2021-12-09 08:17:56 +0100

    fixup! CSI::Grammar / Actions - common CSI::Grammar actions

--- lib/CSI/Grammar/Actions.pm
+++ lib/CSI/Grammar/Actions.pm
@@ -1,14 +1,11 @@
 
 use v5.14;
-use Syntax::Construct 1.008 qw[ package-version package-block ];
-
-use strict;
 use warnings;
 
 package CSI::Grammar::Actions v1.0.0 {
 	use Grammar::Parser::Action::Util (
 		{ action_prefix => 'rule_' },
-		action_name  => { as => 'action_name' },
+		action_name   => { as => 'action_name' },
 
 		alias         => { is => 'alias' },
 		default       => { is => 'default' },
