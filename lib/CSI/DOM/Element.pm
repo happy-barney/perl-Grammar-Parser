1b0ab8902a2d3d1cda8540f5b1c4319295826d15
Branislav Zahradník <happy.barney+perl-Grammar-Parser@gmail.com>
2021-12-09 08:35:53 +0100

    fixup! CSI::Grammar / [WIP] CSI::DOM

--- lib/CSI/DOM/Element.pm
+++ lib/CSI/DOM/Element.pm
@@ -1,21 +1,18 @@
 
 use v5.14;
-use Syntax::Construct v1.8 qw[ package-version package-block ];
-
-use strict;
 use warnings;
 
 package CSI::DOM::Element v1.0.0 {
 	use Moo;
 
-	has children => (
-		is => 'ro',
-		default => sub { +[] },
-	);
+	has 'children'
+		=> is       => 'ro'
+		=> default  => sub { +[] }
+	;
 
-	has parent => (
-		is => 'ro',
-	);
+	has 'parent'
+		=> is       => 'ro'
+	;
 
 	1;
 };
