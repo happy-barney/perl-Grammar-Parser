82c066bbfa3e9ec2ae25ec5c26c8d6ccc65c7b1e
Branislav Zahradník <happy.barney+perl-Grammar-Parser@gmail.com>
2021-12-09 08:30:56 +0100

    fixup! CSI::Grammar / Meta - grammar data holder

--- lib/CSI/Grammar/Meta.pm
+++ lib/CSI/Grammar/Meta.pm
@@ -1,88 +1,75 @@
 
 use v5.14;
-use Syntax::Construct v1.8 qw[ package-version package-block ];
-
-use strict;
 use warnings;
 
 package CSI::Grammar::Meta v1.0.0 {
 	use Moo;
 
-	has class => (
-		is => 'ro',
-	);
+	has 'for_class'
+		=> is       => 'ro'
+		=> init_arg => 'for_class'
+	;
 
-	has grammar => (
-		is => 'ro',
-		init_arg => undef,
-		default => sub { +{} },
-	);
+	has 'grammar'
+		=> is       => 'ro'
+		=> init_arg => undef
+		=> default  => sub { +{} }
+	;
 
-	has _dom => (
-		is => 'ro',
-		init_arg => undef,
-		default => sub { +{} },
-	);
+	has 'dom'
+		=> is       => 'ro'
+		=> init_arg => undef
+		=> default  => sub { +{} }
+	;
 
-	has actions => (
-		is => 'ro',
-		init_arg => undef,
-		default => sub { +{} },
-	);
+	has 'actions'
+		=> is       => 'ro'
+		=> init_arg => undef
+		=> default  => sub { +{} }
+	;
 
-	has insignificant => (
-		is => 'ro',
-		init_arg => undef,
-		default => sub { +[] },
-	);
+	has 'insignificant'
+		=> is       => 'ro'
+		=> init_arg => undef
+		=> default  => sub { +[] }
+	;
 
-	has action_lookup => (
-		is => 'ro',
-		init_arg => undef,
-		default => sub { +[qw[ CSI::Grammar::Actions ]] },
-	);
+	has 'action_lookup'
+		=> is       => 'ro'
+		=> init_arg => undef
+		=> default  => sub { +[qw[ CSI::Grammar::Actions ]] }
+	;
 
-	has default_rule_action => (
-		is => 'rw',
-		init_arg => undef,
-		default => sub { 'default' },
-	);
+	has 'default_rule_action'
+		=> is       => 'rw'
+		=> init_arg => undef
+		=> default  => sub { 'default' }
+	;
 
-	has default_token_action => (
-		is => 'rw',
-		init_arg => undef,
-		default => sub { 'literal' },
-	);
+	has 'default_token_action'
+		=> is       => 'rw'
+		=> init_arg => undef
+		=> default  => sub { 'literal' }
+	;
 
-	has start => (
-		is => 'rw',
-	);
-
-	has rule_name_order_check => (
-		is => 'rw',
-		init_arg => undef,
-		default => sub { 0 },
-	);
-
-	has last_rule => (
-		is => 'rw',
-		init_arg => undef,
-	);
+	has 'start'
+		=> is       => 'rw'
+	;
 
 	sub add_rule {
 		my ($self, $rule, $def) = @_;
 
-		if ($self->rule_name_order_check) {
-			warn "Rule $rule should be defined before ${\ $self->last_rule }"
-				if $rule lt $self->last_rule;
-			$self->last_rule ($rule);
-		}
+		die "Rule '$rule' already defined"
+			if $self->rule_exists ($rule);
 
 		$self->grammar->{$rule} = $def;
 	}
 
 	sub append_rule {
 		my ($self, $rule, $def) = @_;
+
+		die "Rule '$rule' not defined yet"
+			unless $self->rule_exists ($rule);
 
 		my $append = $self->grammar->{$rule};
 
@@ -134,19 +121,6 @@ package CSI::Grammar::Meta v1.0.0 {
 		my ($self, $rule) = @_;
 
 		$self->_dom->{$rule};
-	}
-
-	sub ensure_rule_name_order {
-		my ($self) = @_;
-
-		$self->rule_name_order_check (1);
-		$self->last_rule ('');
-	}
-
-	sub reset_rule_name_order {
-		my ($self) = @_;
-
-		$self->rule_name_order_check (0);
 	}
 
 	1;
